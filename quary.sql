d2e97e0b167b392671891dd1cac1987a42c6f89b
abhibs <abhirambs97@gmail.com>
2024-12-11 14:23:47 +0530

    where with > operator using character

--- quary.sql
+++ quary.sql
@@ -339,3 +339,12 @@ id	    name	    city	        pin
 1	    Abhiram	    Javalli	        577226	
 2	    Anjan	    Balekoppa	    577200	
 3	    Aravind	    Honnekoppa	    577300	
+
+
+SELECT * FROM tabletwo WHERE city > 'C';
+
+id	    name	    city	        pin	
+1	    Abhiram	    Javalli	        577226	
+3	    Aravind	    Honnekoppa	    577300	
+4	    Anitha	    Honnekoppa	    577300	
+5	    Amith	    Thirthahalli	NULL	
